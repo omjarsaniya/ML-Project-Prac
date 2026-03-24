9da958a2c0723d04dc03d803b11ccba09ad08cf2
Om Jarsaniya <ombjarsaniya123@gmail.com>
2026-03-24 18:06:18 +0530

    Data Transformation

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingition import DataIngestion
 from src.mlproject.components.data_ingition import DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformation
+from src.mlproject.components.data_transformation import DataTransformationConfig
 
 
 if __name__ == "__main__":
@@ -11,8 +13,13 @@ if __name__ == "__main__":
     try:
         #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
-        
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config = DataTransformationConfig()
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation(train_path=train_data_path, test_path=test_data_path)
+
+
     except Exception as e:
         logging.info("An exception has occurred")
         raise CustomException(e, sys)
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -6,7 +6,10 @@ import pandas as pd
 from dotenv import load_dotenv # pyright: ignore[reportMissingImports]
 import pymysql # pyright: ignore[reportMissingModuleSource]
 
-
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 load_dotenv()
@@ -33,4 +36,20 @@ def read_sql_data():
     except Exception as e:
         raise CustomException(e,sys)
     
+
+
+def save_object(file_path, obj):
+    try:
+        logging.info("Saving object started")
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+        
+        logging.info(f"Object saved successfully at {file_path}")
+    
+    except Exception as e:
+        logging.error(f"Error saving object: {e}")
+        raise CustomException(e, sys)
     
